c00153d3487e06187acbc7e47bf872e48db6516b
Maary <24504742+Steve-Mr@users.noreply.github.com>
2026-08-21 17:46:07 +0800

    fix(settings): add missing android.os.Build import

--- app/src/main/java/top/maary/darkbag/fragments/SettingsFragment.kt
+++ app/src/main/java/top/maary/darkbag/fragments/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
